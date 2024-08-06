d0f4d837746ce1b7dc396d8aec6d0266ac3aaa70
Dean Xu <xuda.it@outlook.com>
2024-08-06 17:48:49 +0800

    suitable for new sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import {useCallback, useEffect, useRef, useState} from 'react'
 import './App.css'
-import {generateKey, getResult, submitTask, uploadData} from "@xudean/pado-ao-sdk/";
+import {generateKey, getResult, submitTask, uploadData} from "@padolabs/pado-ao-sdk/";
 import Arweave from "arweave";
 // import {submitDataToAR} from "@xudean/pado-ao-sdk/dist/padoarweave.js";
 // import { genArweaveAPI } from "arseeding-js";
 import {getWalletBalance, logTokenTag, printFee, uploadDataByArseeding} from "./script/arseeding.js";
 import {Input, Select, Spin} from "antd";
+import {PadoNetworkContractClient, StorageType} from '@padolabs/pado-ao-sdk'
+
 // import {Everpay} from 'everpay'
 import Everpay from 'everpay'
+import {ethers} from "ethers";
 
 //import {generateKey, getResult, submitTask} from "../../../padolabs/ao/pado-ao-sdk/src/index";
 
@@ -15,8 +18,10 @@ import Everpay from 'everpay'
 function App() {
     const [cliecked, setCliecked] = useState()
     const [address, setAddress] = useState()
+    const [metamaskAddress, setMetamaskAddress] = useState()
     const [fileContent, setFileContent] = useState('');
     const [storageType, setStorageType] = useState('arweave');
+    const [chainName, setChainName] = useState('holesky');
     const [fileContent2, setFileContent2] = useState('');
     const [fileContent3, setFileContent3] = useState('');
     const fileInputRef = useRef(null);
@@ -36,6 +41,10 @@ function App() {
     const [taskMsg, setTaskMsg] = useState(null)
     const tag = "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543"
     const storageTypeOps = [{value: 'arweave', label: 'arweave'}, {value: 'arseeding', label: 'arseeding'},]
+    const supportChains = [{value: 'holesky', label: 'holesky'}, {value: 'ao', label: 'ao'}, {
+        value: 'ethereum',
+        label: 'ethereum'
+    }]
     const [everpayBalance, setEverpayBalance] = useState(null)
 
     const printTokenTag = async () => {
@@ -94,34 +103,27 @@ function App() {
         console.log("connect success!")
     }
 
+    const connectMetamask = async () => {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner()
+        const address = await signer.getAddress();
+        setMetamaskAddress(address);
+        console.log(address)
+    }
+
     const handleFileChange = async (event) => {
-        debugger
         await setIsUploading(true)
         try {
             const file = event.target.files[0];
             if (file) {
                 const reader = new FileReader();
-                reader.readAsArrayBuffer(file); // 读取文件为文本
+                reader.readAsArrayBuffer(file);
                 reader.onload = async (e) => {
                     const content = e.target.result;
                     setFileContent(content);
-                    console.log(content.byteLength); // 打印文件内容到控制台
-                    console.log(content); // 打印文件内容到控制台
-                    const data = new Uint8Array(content);
-                    // tag for the data
-                    let dataTag = {"testtagkey": "testtagvalue"};
-
-                    // price for the data
-                    let priceInfo = {price: "1", symbol: "AOCRED"};
-
-                    const extParam = {
-                        uploadParam: {
-                            storageType: storageType, symbolTag: selectedSymbol
-                        }
-                    }
-                    const dataId = await uploadData(data, dataTag, priceInfo, window.arweaveWallet, arweave, extParam);
-                    console.log(`DATAID=${dataId}`);
-                    setDataId(dataId)
+                    console.log(content.byteLength);
+                    console.log(content);
                     await setIsUploading(false)
                 };
             }
@@ -132,6 +134,31 @@ function App() {
         }
     };
 
+    async function submitData() {
+        if (!fileContent) {
+            throw new Error('no content');
+        }
+        const data = new Uint8Array(fileContent);
+        // tag for the data
+        let dataTag = {"testtagkey": "testtagvalue"};
+
+        // price for the data
+        let priceInfo = {price: "1", symbol: "AOCRED"};
+        debugger
+        //chainName will provided by caller
+        const wallets = {
+            wallet: window.ethereum,
+            storageWallet: window.arweaveWallet
+        }
+        const padoNetworkClient = new PadoNetworkContractClient(chainName, storageType, wallets);
+
+        const dataId = await padoNetworkClient.uploadData(data, dataTag, priceInfo);
+
+        // upload your data (If you want to do a local test, refer to the README to initialize arweave and then pass it to uploadData)
+        console.log(`DATAID=${dataId}`);
+        setDataId(dataId)
+    }
+
 
     function clearFile() {
         fileInputRef.current.value = '';
@@ -140,6 +167,11 @@ function App() {
     function handleStorageTypeChnage(value) {
         console.log('choose type:', value)
         setStorageType(value)
+    }
+
+    function handleChainNameChnage(value) {
+        console.log('choose chain:', value)
+        setChainName(value)
     }
 
     async function handleSymbolChange(value) {
@@ -152,23 +184,13 @@ function App() {
 
 
     async function submitTaskAndGetResult() {
-        setTaskMsg('generate key')
-        let key = await generateKey();
-        // console.log('dataInfo:',JSON.parse(JSON.parse(dataInfo).dataTag))
-        setTaskMsg('submit task')
-        const taskId = await submitTask(userDataId, key.pk, window.arweaveWallet);
-        console.log(`TASKID=${taskId}`);
-        setTaskMsg('get task result')
-        const [err, data] = await getResult(taskId, key.sk).then(data => {
-            setTaskMsg(null)
-            return [null, data]
-        }).catch(err => {
-            setTaskMsg(null)
-            alert(err)
-            return [err, null]
-        });
-        console.log(`err=${err}`);
-        console.log(`data=${data}`);
+        const wallets = {
+            wallet: window.ethereum,
+            storageWallet: window.arweaveWallet
+        }
+        const padoNetworkClient = new PadoNetworkContractClient(chainName, storageType, wallets);
+        const taskId = await padoNetworkClient.submitTask('',userDataId)
+        const data = await padoNetworkClient.getTaskResult(taskId);
         //for test
         if (data) {
             downloadArrayBufferAsFile(data, 'raw_data_file')
@@ -201,13 +223,46 @@ function App() {
         <h2>Connect Wallet</h2>
         <div className="card">
             <button disabled={cliecked} onClick={connectWallet}>
-                Connect
+                Connect ArConnect
             </button>
             <br/>
             {address && <a>{address}</a>}
             <br/>
             {arweaveBalance && <a>AR:{arweaveBalance}</a>}
         </div>
+        <div className="card2">
+            <button disabled={cliecked} onClick={connectMetamask}>
+                Connect Metamask
+            </button>
+            <br/>
+            {metamaskAddress && <a>{metamaskAddress}</a>}
+            {/*<br/>*/}
+            {/*{arweaveBalance && <a>AR:{arweaveBalance}</a>}*/}
+        </div>
+        <hr/>
+        <h2>Choose Chain and Storage Type</h2>
+        <div>
+            chainName:<Select style={{width: '200px'}} options={supportChains}
+                              onChange={handleChainNameChnage}
+                              defaultValue={chainName}></Select>
+        </div>
+        <div>
+            storageType:<Select style={{width: '200px'}} options={storageTypeOps}
+                                onChange={handleStorageTypeChnage}
+                                defaultValue={storageType}></Select>
+        </div>
+        {storageType === "arseeding" && (
+            <div>
+                symbols:<Select style={{width: '200px'}} options={arseedingSymbols}
+                                defaultValue={arseedingSymbols[0]} onChange={handleSymbolChange}></Select>
+            </div>
+
+        )
+        }
+        {
+            everpayBalance && (<div>balance: {everpayBalance}</div>)
+        }
+
         <hr/>
         <div style={{
             display: 'flex',
@@ -216,23 +271,6 @@ function App() {
         }}>
             <div className="card">
                 <h2>Upload File(Data Provider)</h2>
-                <div>
-                    storageType:<Select style={{width: '200px'}} options={storageTypeOps}
-                                        onChange={handleStorageTypeChnage}
-                                        defaultValue={storageType}></Select>
-                </div>
-                {storageType === "arseeding" && (
-                    <div>
-                        symbols:<Select style={{width: '200px'}} options={arseedingSymbols}
-                                        defaultValue={arseedingSymbols[0]} onChange={handleSymbolChange}></Select>
-                    </div>
-
-                )
-                }
-                {
-                    everpayBalance && (<div>balance: {everpayBalance}</div>)
-                }
-
                 <div className="card">
                     <input type="file"
                            name="myFile"
@@ -242,6 +280,9 @@ function App() {
                 </div>
                 {isUploading && <Spin tip={'Uploading...'} size="small">Uploading</Spin>}
                 {dataId && <div>DATAID: <a style={{color: 'red'}}>{dataId}</a></div>}
+                <button onClick={submitData}>submitData</button>
+
+
             </div>
             <div className="card">
                 <h2>Submit Task(Data User)</h2>
